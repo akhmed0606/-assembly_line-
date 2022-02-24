ef122c323255634f6b2c4c6a47f87e9dfd81064e
akhmed0606 <analgiev57gmail.com>
2022-02-24 17:43:44 -0500

    create state variables & set value to empty array

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+import { useState,useRef } from 'react'
+import { Istages } from './interface';
 import './App.css';
+
+const Assembline = ( {stages }: Istages) => {
+  const [tasks, setTasks] = useState<string[]>([])
+}
 
 function App() {
   return (
     <div className="App">
       <button className='btnAdd'>Add an item:</button>
-     <input type='text' id='taskInput'/>
+     <input type='text' id='taskInput' ref={taskRef}/>
     </div>
   );
 }

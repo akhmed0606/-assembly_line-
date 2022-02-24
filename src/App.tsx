9d603a3dbc9f3a13eb5017fdfa14aa94665e81f4
akhmed0606 <analgiev57gmail.com>
2022-02-24 17:17:25 -0500

    create input for task and button,add basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-
+import { useState } from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      HELLO WORLD
+      <button className='btnAdd'>Add an item:</button>
+     <input type='text' id='taskInput'/>
     </div>
   );
 }

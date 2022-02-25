9c3974ed03dcdb8b04099330283f8ef5e5b89b77
akhmed0606 <analgiev57gmail.com>
2022-02-24 21:40:09 -0500

    move to next stage if array is not last one

--- src/App.tsx
+++ src/App.tsx
@@ -18,67 +18,78 @@ const AssemblyLine = ({ stages }: Istages) => {
 
   return (
     <>
-    <div>
-      <button
-        onClick={() => {
-          if (stages.length === 0) {
-            return;
-          }
-          setTasks((tasks) => [[...tasks[0], task], ...tasks.slice(1)]);
-          setTask(""); 
-        }}
-        className='btnAdd'
-      >
-        Add an item:
-      </button>
-      <input type="text" id="taskInput" onChange={handleChange} value={task} />
-      {task}
+      <div>
+        <button
+          onClick={() => {
+            if (stages.length === 0) {
+              return;
+            }
+            setTasks((tasks) => [[...tasks[0], task], ...tasks.slice(1)]);
+            setTask("");
+          }}
+          className="btnAdd"
+        >
+          Add an item:
+        </button>
+        <input
+          type="text"
+          id="taskInput"
+          onChange={handleChange}
+          value={task}
+        />
       </div>
-      {stages.map((stage,i) => {
+      {stages.map((stage, i) => {
         return (
-          
-          <div key={`${stage}-${i}`} className='stageContainer'>
-            <div className='stages'>
-            <h3>{stage}</h3>
+          <div key={`${stage}-${i}`} className="stageContainer">
+            <div className="stages">
+              <h3>{stage}</h3>
             </div>
 
-         {(tasks[i] ?? [])?.map((task: string, j: string) => (
-           <button
-           key={`${task}-${j}`}
-           onClick={() => {
-             setTasks((tasks) =>
-             i === 0 ? [
-               ...tasks.slice(0,i),
-               [...tasks[i].slice(0,j), ...tasks[i].slice(j + 1)], ...tasks.slice(i + 1)
-             ]
-             : [...tasks.slice(0, i - 1),
-            [...tasks[i - 1], task],
-            [...tasks[i].slice(0,j), ...tasks[i].
-             slice(j + 1)], ...tasks.slice(i + 1)
-            ]
-             )
-           }}
-           onContextMenu={() => {
-             setTasks((tasks) => 
-                i === tasks.length - 1 ?
-                [...tasks.slice(0, i),
-                [...tasks[i].slice(0, i), ...tasks[i].slice(i + 1)],
-                ...tasks.slice(i + 1)
-                ]
-                : [
-                  
-                ]
-             )
-           }}
-           >
-
-           </button>
-         ))}
-        </div>
-        )
-      
+            {(tasks[i] ?? [])?.map((task: string, j: string) => (
+              <button
+                key={`${task}-${j}`}
+                onClick={() => {
+                  setTasks((tasks) =>
+                    i === 0
+                      ? [
+                          ...tasks.slice(0, i),
+                          [...tasks[i].slice(0, j), ...tasks[i].slice(j + 1)],
+                          ...tasks.slice(i + 1),
+                        ]
+                      : [
+                          ...tasks.slice(0, i - 1),
+                          [...tasks[i - 1], task],
+                          [...tasks[i].slice(0, j), ...tasks[i].slice(j + 1)],
+                          ...tasks.slice(i + 1),
+                        ]
+                  );
+                }}
+                onContextMenu={() => {
+                  setTasks((tasks) =>
+                    i === tasks.length - 1
+                      ? [
+                          ...tasks.slice(0, i),
+                          [...tasks[i].slice(0, j), ...tasks[i].slice(j + 1)],
+                          ...tasks.slice(i + 1),
+                        ]
+                      : [
+                          ...tasks.slice(0, i),
+                          [
+                            ...tasks[i].slice(0, j),
+                            ...tasks[i].slice(j + 1),
+                            task,
+                          ],
+                          ...tasks.slice(i + 1),
+                        ]
+                  );
+                }}
+              >
+                {task}
+              </button>
+            ))}
+          </div>
+        );
       })}
-     
     </>
   );
 };

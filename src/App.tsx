99e6b20a045d64c7200f61c5d54763baf15863f6
akhmed0606 <analgiev57gmail.com>
2022-02-24 19:48:44 -0500

    remove onEnter func doesn't work with button

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,45 @@
-import { useState,useEffect,KeyboardEvent } from 'react'
-import { Istages } from './interface';
-import './App.css';
+import { useState, useEffect, KeyboardEvent } from "react";
+import { Istages } from "./interface";
+import "./App.css";
 
-const AssemblyLine = ( {stages }: Istages) => {
-  const [tasks, setTasks] = useState<any[]>([])
-  const [task, setTask] = useState<any>("")
- 
- useEffect(() => {
-   setTasks(stages.map(() => []) as any)
- },[stages])
+const AssemblyLine = ({ stages }: Istages) => {
+  const [tasks, setTasks] = useState<any[]>([]);
+  const [task, setTask] = useState<string>("");
 
-  const handleChange = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
+  useEffect(() => {
+    setTasks(stages.map(() => []) as any);
+  }, [stages]);
+
+  const handleChange = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
     setTask(value);
-    
   };
- 
-  const onEnter = (e: KeyboardEvent) => {
-    if (e.key === 'Enter'){
-     if (stages.length === 0)
-      return;
-    }
-     setTasks((tasks): any => [[...tasks[0], task], ...tasks.slice(1)])
-     setTask("")
-  }
-  
+
   return (
     <>
-     <button onClick={() => {if (stages.length === 0) {
-              return;
-            }
-            setTasks((tasks) => [[...tasks[0], task], ...tasks.slice(1)]);
-            setTask("")}} className='btnAdd'>Add an item:</button>
-     <input type='text' id='taskInput' onChange={handleChange} value={task} onKeyPress={onEnter} />
-     {task}
+      <button
+        onClick={() => {
+          if (stages.length === 0) {
+            return;
+          }
+          setTasks((tasks) => [[...tasks[0], task], ...tasks.slice(1)]);
+          setTask(""); 
+        }}
+        className='btnAdd'
+      >
+        Add an item:
+      </button>
+      <input type="text" id="taskInput" onChange={handleChange} value={task} />
+      {task}
     </>
-  )
-}
+  );
+};
 
 function App() {
   return (
     <div className="App">
-       <AssemblyLine stages={["Idea", "Development", "Testing", "Deployment"]} />
+      <AssemblyLine stages={["Idea", "Development", "Testing", "Deployment"]} />
     </div>
   );
 }

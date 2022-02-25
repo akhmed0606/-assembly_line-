fffd8d081b9b17b8a505ec379bd397bab122a7ba
akhmed0606 <analgiev57gmail.com>
2022-02-24 21:03:34 -0500

    remove first array after left click

--- src/App.tsx
+++ src/App.tsx
@@ -44,8 +44,16 @@ const AssemblyLine = ({ stages }: Istages) => {
 
          {(tasks[i] ?? [])?.map((task: string, j: string) => (
            <button
-           
-           
+           key={`${task}-${j}`}
+           onClick={() => {
+             setTasks((tasks) =>
+             i === 0 ? [
+               ...tasks.slice(0,i),
+               [...tasks[i].slice(0,j), ...tasks[i].slice(j + 1)], ...tasks.slice(i + 1)
+             ]
+             : []
+             )
+           }}
            >
 
            </button>
